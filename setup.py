9c55a15366a5c3a2472495a1ae16d4eec4b969c2
rodeq <rodrigo@codeq.com>
2020-03-09 10:15:05 +0100

    Release 1.13.0.

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 
-__version__ = "1.12.0"
+__version__ = "1.13.0"
 
 download_url = "https://github.com/codeqLLCdev/codeq-api-sdk/archive/v%s.tar.gz" % __version__
 
